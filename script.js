308d1209304d0c7ee953586236a644e8151462e1
Stephen Obie <sobie31@outlook.com>
2021-04-24 20:45:07 -0400

    update js

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
-var startBtn = document.getElementById("start-btn")
-var questionContainerEl = document.getElementById("question-container")
-var questionEl = document.getElementById("#question")
+var startBtn = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerButtonEl = document.getElementById ("answer-buttons")
+var timerEl = document.getElementById("countdown");
 
-var questions = [
-    { question: "What is Michael Scott's middle name?",
-      answers: [
-          {text: "Larry", correct: false },
-          {text: "Gary", correct: true },
-          {text: "James", correct: false },
-          {text: "Ryan", correct: false },
-      ]  
-    }
-]
+var questions = 
+[
+  {
+    question: "What is Michael Scott's middle name?",
+    choices: ["Larry", "Brian", "Gary", "James"],
+    answer: 2
+  },
+  
+  {
+    question: "Where does The Office take place?",
+    choices: ["Los Angeles", "Scranton", "Miami", "New York"],
+    answer: 1
+  },
+  {
+   question: "What size Chicken pot-pie does Michael eat?",
+   choices: ["Small", "Regular", "Large", "Family-Size"],
+   answer: 3
+  },
+  {
+    question: "What season does the Dinner Party episode take place?",
+    choices: ["Season 1", "Season 5", "Season 3", "Season 4"],
+    answer: 3
+  },
+  {
+    question: "Where does Jim propose to Pam?",
+    choices: ["Breakroom", "Gas Station", "Parking Lot", "Restaurant"],
+    answer: 1
+  }
+];
 startBtn.addEventListener("click", startGame)
 
 function startGame() {
+    startBtn.classList.add("hide")
+    questionContainerEl.classList.remove("hide")
     
 }
-function nextQuestion() {
     
+
+function showQuestions(question) {
+  questionEl.innerText = question.question
 }
 function selectAnswer() {
     
 }
 
 
-var count = 60, timer = setInterval(function() {
-$('#timer').html(count--);
-if(count == 1) clearInterval(timer);
-}, 1000);
+function countdown() {
+  var timeLeft = 60;
+
+  
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+countdown();
 
 
 
