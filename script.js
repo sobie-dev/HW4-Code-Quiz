0e46442ef7dec723445501c98abac248e2864ceb
Stephen Obie <sobie31@outlook.com>
2021-04-24 21:08:09 -0400

    update js

--- script.js
+++ script.js
@@ -33,6 +33,28 @@ var questions =
     answer: 1
   }
 ];
+
+for ( var i = 0; i < questions.length; i++ ) {
+  var question = questions[i].question;
+  document.write ( question );
+  var options = questions[i].choices;
+  document.body.appendChild(document.createElement("br"));
+  var name = "radio"+i; 
+  for ( var opt in options ) {
+    
+    var radioEle = document.createElement("input");
+    radioEle.type = "radio";          
+    radioEle.value = options[opt];
+    document.body.appendChild(radioEle);
+    var label = document.createElement("Label");
+    label.innerHTML = options[opt];
+    document.body.appendChild(label);
+    document.body.appendChild(document.createElement("br"));
+  }
+  
+  document.body.appendChild(document.createElement("br"));
+  
+}
 startBtn.addEventListener("click", startGame)
 
 function startGame() {
@@ -41,13 +63,12 @@ function startGame() {
     
 }
     
+function renderProgress(){
+  for(var qIndex = 0; qIndex <= lastQuestion; qIndex++){
+    ProgressEvent.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+  }
+}
 
-function showQuestions(question) {
-  questionEl.innerText = question.question
-}
-function selectAnswer() {
-    
-}
 
 
 function countdown() {
