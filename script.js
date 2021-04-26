4b21708e3d534611b8576ec9252ee79777b38bdd
Stephen Obie <sobie31@outlook.com>
2021-04-25 21:06:29 -0400

    added timer and got the questions to come up

--- script.js
+++ script.js
@@ -1,97 +1,165 @@
-var startBtn = document.getElementById("start-btn");
-var questionContainerEl = document.getElementById("question-container");
-var questionEl = document.getElementById("question");
-var answerButtonEl = document.getElementById ("answer-buttons")
+
+
 var timerEl = document.getElementById("countdown");
 
-var questions = 
+var startBox = document.getElementById('startBox')
+var quizBox = document.getElementById('quizBox')
+var message = document.getElementById("message")
+var userInitialsBox = document.getElementById('userInitialsBox')
+var highscoresBox = document.getElementById('highscoresBox')
+
+var timerEl = document.getElementById('timer');
+var startButton = document.getElementById('startBtn');
+
+var quizQuestions = 
 [
   {
     question: "What is Michael Scott's middle name?",
     choices: ["Larry", "Brian", "Gary", "James"],
-    answer: 2
+    correctAnswer: "c"
   },
   
   {
     question: "Where does The Office take place?",
     choices: ["Los Angeles", "Scranton", "Miami", "New York"],
-    answer: 1
+    correctAnswer: "b"
   },
   {
    question: "What size Chicken pot-pie does Michael eat?",
    choices: ["Small", "Regular", "Large", "Family-Size"],
-   answer: 3
+   correctAnswer: "d"
   },
   {
     question: "What season does the Dinner Party episode take place?",
     choices: ["Season 1", "Season 5", "Season 3", "Season 4"],
-    answer: 3
+    correctAnswer: "d"
   },
   {
     question: "Where does Jim propose to Pam?",
     choices: ["Breakroom", "Gas Station", "Parking Lot", "Restaurant"],
-    answer: 1
+    correctAnswer: "b"
   }
 ];
+var quizIndex = 0
+var score;
 
-for ( var i = 0; i < questions.length; i++ ) {
-  var question = questions[i].question;
-  document.write ( question );
-  var options = questions[i].choices;
-  document.body.appendChild(document.createElement("br"));
-  var name = "radio"+i; 
-  for ( var opt in options ) {
-    
-    var radioEle = document.createElement("input");
-    radioEle.type = "radio";          
-    radioEle.value = options[opt];
-    document.body.appendChild(radioEle);
-    var label = document.createElement("Label");
-    label.innerHTML = options[opt];
-    document.body.appendChild(label);
-    document.body.appendChild(document.createElement("br"));
-  }
-  
-  document.body.appendChild(document.createElement("br"));
-  
-}
-startBtn.addEventListener("click", startGame)
+startButton.addEventListener('click', () => {
+    startBox.classList.add("hide")
+    quizBox.classList.remove("hide")
 
-function startGame() {
-    startBtn.classList.add("hide")
-    questionContainerEl.classList.remove("hide")
-    
-}
-    
-function renderProgress(){
-  for(var qIndex = 0; qIndex <= lastQuestion; qIndex++){
-    ProgressEvent.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-  }
-}
+    askQuestion();
+    startCountdown();
+});
+function askQuestion() {
+    if (quizIndex < quizQuestions.length) {
+        quizBox.innerHTML = ""
+        var questionObj = quizQuestions[quizIndex]
 
+        var questionEl = document.createElement("div")
+        questionEl.textContent = questionObj.question
+        quizBox.appendChild(questionEl)
 
-
-function countdown() {
-  var timeLeft = 60;
-
-  
-  var timeInterval = setInterval(function () {
-    if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
+        for (var choice of questionObj.choices) {             
+            var btn = document.createElement("button")
+            btn.textContent = choice
+            btn.addEventListener("click", handleChoice)
+            quizBox.appendChild(btn)
+        }
     } else {
-      timerEl.textContent = '';
-      clearInterval(timeInterval);
-      displayMessage();
+        
+        endGame();
     }
-  }, 1000);
+    function handleChoice(event) {
+        var chosen = event.target.value;
+        if (chosen !== quizQuestions[quizIndex].correctAnswer) {
+            timeLeft = timeLeft - 10;
+        }
+        
+        quizIndex++;
+        askQuestion()
 }
-countdown();
+
+function endGame () {
+    clearInterval(timeInterval)
+    timerEl.style.display = "none"
+    
+    quizBox.classList.add("hide")
+    userInitialsBox.classList.remove("hide")
+    highscoresBox.classList.remove("hide")
+    score = timeLeft;
+
+    highscoresBox.innerHTML = ""
+    
+    var highScoresEl = document.createElement("div")
+    highScoresEl.textContent = "Your score is: " + score
+    highscoresBox.appendChild(highScoresEl)
+
+   
+    userInitialsBox.innerHTML = ""
+    var initialsEl = document.createElement("input")
+    initialsEl.type = ("form")
+    userInitialsBox.appendChild(initialsEl)
+
+    var submit = document.createElement("button")
+    submit.textContent = "Submit"
+    userInitialsBox.appendChild(submit)
+
+    submit.addEventListener("click", submitScore)
+}
 
 
+
+    
+}
+var timeLeft;
+var timeInterval;
+
+function startCountdown() {
+    timeLeft = 60;
+    timeInterval = setInterval(function () {
+        timerEl.textContent = "Timer: " + timeLeft;
+        timeLeft--;
+      if (timeLeft < 1) {
+            timerEl.textContent = 'Time is up!';
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
+}
+
+var initialsEl;
+function endGame () {
+    
+    clearInterval(timeInterval)
+    timerEl.style.display = "none"
+    
+    quizBox.classList.add("hide")
+    userInitialsBox.classList.remove("hide")
+    highscoresBox.classList.remove("hide")
+    
+    score = timeLeft;
+
+    highscoresBox.innerHTML = ""
+    const highScoresEl = document.createElement("div")
+    highScoresEl.textContent = "Your score is: " + score
+    highscoresBox.appendChild(highScoresEl)
+
+    userInitialsBox.innerHTML = ""
+    initialsEl = document.createElement("input")
+    initialsEl.setAttribute("id", "user-initials")
+    userInitialsBox.appendChild(initialsEl)
+
+    const submit = document.createElement("button")
+    submit.textContent = "Submit"
+    userInitialsBox.appendChild(submit)
+
+    submit.addEventListener("click", submitScore)
+}
+
+function submitScore() {
+    var inputInitials = document.querySelector("#user-initials").value;
+    localStorage.setItem("initials", inputInitials)
+}
 
 
 
